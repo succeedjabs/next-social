b29480b9961389b71c3f581d423092d3e14e22d3
succeedjabs <succeedjabs@gmail.com>
2024-09-01 16:29:43 -0500

    implementing authentication

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import Link from "next/link"
 import MobileMenu from "./MobileMenu"
 import Image from "next/image"
+import { ClerkLoaded, ClerkLoading, SignedIn, SignedOut, UserButton } from "@clerk/nextjs"
+
 
 const Navbar = () => {
     
@@ -11,7 +13,7 @@ const Navbar = () => {
             <Link href="/" className="font-bold text-xl text-blue-600">LAMASOCIAL</Link>
         </div>
         {/* CENTER */}
-        <div className="hidden md:flex w-[50%] text-sm">
+        <div className="hidden md:flex w-[50%] text-sm items-center justify-between">
             {/*  LINKS */}
             <div className="flex gap-6 text-gray-600">
                 <Link href="/" className="flex items-center gap-2">
@@ -27,9 +29,37 @@ const Navbar = () => {
                     <span>Stories</span>
                 </Link>
             </div>
+            <div className="hidden xl:flex p-2 bg-slate-100 items-center rounded-lg">
+                <input type="text" placeholder="search..." className="bg-transparent outline-none"/>
+                <Image src="/search.png" alt="" width={14} height={14}/>
+            </div>
         </div>
         {/* RIGHT */}
         <div className="w-[30%] flex items-center gap-4 xl:gap-8 justify-end">
+            <ClerkLoading>
+                <div className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current 
+                border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white" />
+            </ClerkLoading>
+            <ClerkLoaded>
+                <SignedIn>
+                    <div className="cursor-pointer">
+                        <Image src="/people.png" alt="" width={24} height={24} />
+                    </div>
+                    <div className="cursor-pointer">
+                        <Image src="/messages.png" alt="" width={20} height={20} />
+                    </div>
+                    <div className="cursor-pointer">
+                        <Image src="/notifications.png" alt="" width={20} height={20} />
+                    </div>
+                    <UserButton/>
+                </SignedIn>
+                <SignedOut>
+                    <div className="flex items-center gap-2 text-sm">
+                        <Image src="/login.png" alt="" width={20} height={20} />
+                        <Link href="/sign-in">Login/Register</Link>
+                    </div>
+                </SignedOut>
+            </ClerkLoaded>
             <MobileMenu /> 
         </div>
     </div>
